764c5e07905b1d5b6bd8c0049599a46323943459
sakthiofficial <sakthiroky123@gmail.com>
2023-01-10 18:19:23 +0530

    client cmd

--- index.js
+++ index.js
@@ -6,48 +6,9 @@ import userRouter from "./userrouter.js";
 import cors from "cors";
 import sendMail from "./mailSender.js"
 dotenv.config()
-console.time()
-// console.log(process.env.password);
-// Step to start.
 
-// to install node package.
-// npm init -y 
 
-// Instal Express.
-// npm i epress
 
-// To auto restart the portal.
-// install -cmd.
-// npm install --save-dev nodemon
-// past the code in script obt in pack-json
-// ("start": "node index.js", // helps in heroku deployment
-// "dev": "nodemon index.js" // shortcut to run nodemon)
-
-// cmd to run in dev -> npm run dev;
-// cmd to run in horuku -> npm start;
-// import { log, time } from "console";
-// let movies;
-// var app = express();
-dotenv.config()
-
-// Step to start.
-
-// to install node package.
-// npm init -y
-
-// Instal Express.
-// npm i epress
-
-// To auto restart the portal.
-// install -cmd.
-// npm install --save-dev nodemon
-// past the code in script obt in pack-json
-// ("start": "node index.js", // helps in heroku deployment
-// "dev": "nodemon index.js" // shortcut to run nodemon)
-
-// cmd to run in dev -> npm run dev;
-// cmd to run in horuku -> npm start;
-// import { log } from "console";
 let movies;
 const app = express();
 app.use(cors())
@@ -61,18 +22,12 @@ app.use(express.json())
 // MongoDB Connection.
 // const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL
-// const MONGO_URL = "mongodb://127.0.0.1"
 
-// Mongo Connection With a key to use
 const client = new MongoClient(MONGO_URL);
 // dail
 await client.connect(); // call
 
 
-
-
-
-app.listen(4220, () => console.log(`The server started in: ${4020} ✨✨`));
 
 app.use("/user", userRouter)
 
@@ -87,7 +42,9 @@ app.post("/mobiles", async (req, res) => {
 })
 
 
-export { client };
+app.listen(4220, () => console.log(`The server started in: ${4020} ✨✨`));
+// export { client };
+
 
 // param in get method.
 
